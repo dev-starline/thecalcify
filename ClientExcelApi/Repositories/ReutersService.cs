a7e1a05b743d621168903e2b6cadf0beef36849a
Rajesh Sharma <rajesh@starlinetechno.com>
2025-10-13 12:48:47 +0530

    Added Subject Code in NewsDescription endpoint

--- ClientExcelApi/Repositories/ReutersService.cs
+++ ClientExcelApi/Repositories/ReutersService.cs
@@ -292,6 +292,7 @@ namespace Reuters.Repositories
                                     firstCreated
                                     sortTimestamp
                                     contentTimestamp
+                                    subject { code }
                                   }
                                 }";
 
